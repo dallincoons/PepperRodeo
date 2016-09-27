3c52b9b0f8d97f658cc513e46c6b00d025f6e8b5
eddcoons <emilydcoons@gmail.com>
2016-09-27 10:21:21 -0700

    breaks out styling into function specific files

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -12,9 +12,9 @@
 
 <nav class="pr-nav">
     <ul>
-        <li><a><i class="fa fa-cutlery"></i></a></li>
-        <li><a><i class="fa fa-shopping-cart"></i></a></li>
-        <li><a class="active"><i class="fa fa-home"></i></a></li>
+        <li><a href="recipe"><i class="fa fa-cutlery"></i></a></li>
+        <li><a href="grocerylist"><i class="fa fa-shopping-cart"></i></a></li>
+        <li><a href="/" class="active"><i class="fa fa-home"></i></a></li>
         <li><a><i class="fa fa-users"></i></a></li>
         <li><a><i class="fa fa-user"></i></a></li>
     </ul>
