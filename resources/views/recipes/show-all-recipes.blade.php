f3260511c1bedb4bda95c6bdaaf51e7179790636
eddcoons <emilydcoons@gmail.com>
2016-09-27 12:35:54 -0700

    Styling for the recipes page

--- resources/views/recipes/show-all-recipes.blade.php
+++ resources/views/recipes/show-all-recipes.blade.php
@@ -3,19 +3,56 @@
 @section('content')
         <h2 class="page-title">My Recipes</h2>
 
-        <nav class="recipe-mini-nav">
+        <nav class="mini-nav recipe-nav">
              <ul>
-                 <li><a><i class="fa fa-plus"></i></a></li>
+                 <li><a href="#"><i class="fa fa-plus"></i></a></li>
                  <li><a><i class="fa fa-cart-plus"></i></a></li>
                  <li><a><i class="fa fa-pencil"></i></a></li>
-                 <li><a><i class="fa fa-plus"></i></a></li>
-                 <li><a><i class="fa fa-plus"></i></a></li>
+                 <li><a><i class="fa fa-users"></i></a></li>
+                 <li><a><i class="fa fa-trash"></i></a></li>
              </ul>
         </nav>
 
-        <ul>
-        @foreach($recipes as $recipe)
-            <li><a href="recipe/{{$recipe->id}}">{{$recipe->title}}</a></li>
-        @endforeach
-        </ul>
+
+        <div class="category-wrapper">
+            <ul class="category">
+                <li class="category-title"><h3>Category Title</h3></li>
+                <li>
+                    <ul class="recipes">
+                        @foreach($recipes as $recipe)
+                            <li><a href="recipe/{{$recipe->id}}">{{$recipe->title}}</a></li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+            <ul class="category">
+                <li class="category-title"><h3>Category Title</h3></li>
+                <li>
+                    <ul class="recipes">
+                        <li>Placeholder recipe</li>
+                        <li>Placeholder recipe</li>
+                        <li>Placeholder recipe</li>
+                        <li>Placeholder recipe</li>
+                        <li>Placeholder recipe</li>
+                        <li>Placeholder recipe</li>
+                    </ul>
+                </li>
+
+            </ul>
+            <ul class="category">
+                <li class="category-title"><h3>Category Title</h3></li>
+                <li>
+                    <ul class="recipes">
+                        <li>Placeholder recipe</li>
+                        <li>Placeholder recipe</li>
+                        <li>Placeholder recipe</li>
+                        <li>Placeholder recipe</li>
+                        <li>Placeholder recipe</li>
+                        <li>Placeholder recipe</li>
+                    </ul>
+                </li>
+
+            </ul>
+
+        </div>
 @endsection
