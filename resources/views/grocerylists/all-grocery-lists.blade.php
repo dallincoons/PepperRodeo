b175f7cc660281c9a111781bb99b3eaf97bfb135
eddcoons <emilydcoons@gmail.com>
2016-09-28 14:42:03 -0700

    styling for all grocery lists page

--- resources/views/grocerylists/all-grocery-lists.blade.php
+++ resources/views/grocerylists/all-grocery-lists.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.app', ['vue' => 'all-grocery-lists'])
 
 @section('content')
-    <ul>
-    @foreach($grocerylists as $list)
-        <li><a href="grocerylist/{{$list->id}}">{{$list->title}}</a></li>
-    @endforeach
-    </ul>
+    <div class="grocery-lists">
+
+        <h2 class="page-title">My Lists</h2>
+        <nav class="mini-nav">
+            <ul>
+                <li><i class="fa fa-plus"></i></li>
+                <li><i class="fa fa-pencil"></i></li>
+                <li><i class="fa fa-users"></i></li>
+                <li><i class="fa fa-trash"></i></li>
+            </ul>
+        </nav>
+
+        <ul class="lists">
+            @foreach($grocerylists as $list)
+                <li><i class="fa fa-list"></i> <a href="grocerylist/{{$list->id}}">{{$list->title}}</a></li>
+            @endforeach
+        </ul>
+    </div>
+
 @endsection
