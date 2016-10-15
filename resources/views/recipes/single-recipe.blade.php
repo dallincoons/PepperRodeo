5af975e0c425eec3539d7158af0e71dfea8b5ab9
eddcoons <emilydcoons@gmail.com>
2016-10-15 14:39:37 -0700

    styling for individual recipe pages

--- resources/views/recipes/single-recipe.blade.php
+++ resources/views/recipes/single-recipe.blade.php
@@ -1,19 +1,42 @@
 @extends('layouts.app', ['vue' => 'single-recipe'])
 
 @section('content')
-    <h1>{{$recipe->title}}</h1>
-    <ul>
+
+<div class="recipe-wrapper">
+    <h2 class="page-title">{{$recipe->title}}</h2>
+    <h6 class="recipe-cat">Categories: <a>Lunch</a>, <a>Dinner</a></h6>
+    <nav class="mini-nav">
+        <ul>
+            <li><a><i class="fa fa-cart-plus"></i></a></li>
+            <li><a><i class="fa fa-pencil"></i></a></li>
+            <li><a><i class="fa fa-users"></i></a></li>
+            <li><a><i class="fa fa-trash"></i></a></li>
+        </ul>
+    </nav>
+
+    <h3 class="form-heading">Ingredients</h3>
+    <ul class="ingredients">
         @foreach($recipe->items as $item)
             <li>{{$item->quantity}} {{$item->name}}</li>
         @endforeach
     </ul>
-    <h1>Add to Grocery List</h1>
-    <ul>
-    @foreach($listsWithoutRecipe as $grocerylist)
-        {{--<li @click="addToGroceryList({{$grocerylist->getKey()}})">{{$grocerylist->title}}</li>--}}
-        <li><a href="/grocerylist/{{$grocerylist->getKey()}}/add/{{$recipe->getKey()}}">{{$grocerylist->title}}</a></li>
-    @endforeach
+
+    <h3 class="form-heading">Directions</h3>
+
+
+
+    <h5 class="add-list-title page-title">Add to Grocery List</h5>
+    <ul class="lists">
+        @foreach($listsWithoutRecipe as $grocerylist)
+            {{--<li @click="addToGroceryList({{$grocerylist->getKey()}})">{{$grocerylist->title}}</li>--}}
+            <li><i class="fa fa-list"></i> <a href="/grocerylist/{{$grocerylist->getKey()}}/add/{{$recipe->getKey()}}">{{$grocerylist->title}}</a></li>
+        @endforeach
     </ul>
 
-<input type="hidden" value="{{$recipe->getKey()}}" v-model="recipeId">
+    <input type="hidden" value="{{$recipe->getKey()}}" v-model="recipeId">
+
+</div>
+
+
+
 @endsection
