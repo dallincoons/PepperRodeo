f298c2b360c76bc862411d622e8579c97c7caee8
Dallin <dallinis@hotmail.com>
2016-10-15 12:25:06 -0700

    changes all lists button to link

--- resources/views/home/includes/user-home.blade.php
+++ resources/views/home/includes/user-home.blade.php
@@ -18,6 +18,6 @@
         <h3><i class="fa fa-list"></i> <a href="grocerylist/{{$list->getKey()}}">{{$list->title}}</a></h3>
     @endforeach
 
-    <button type="button" class="pr-button pr-button--home-lists">All Lists</button>
+    <a href="/grocerylist" class="pr-button pr-button--home-lists">All Lists</a>
 </div>
 
