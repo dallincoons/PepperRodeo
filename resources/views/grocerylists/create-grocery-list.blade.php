e4cc31e3c899229df8446d366c26c404d3bd6006
Dallin <dallinis@hotmail.com>
2016-10-15 14:35:11 -0700

    fixes save list bug

--- resources/views/grocerylists/create-grocery-list.blade.php
+++ resources/views/grocerylists/create-grocery-list.blade.php
@@ -11,6 +11,7 @@
         <div class="recipes-added">
             <p v-for="recipe in addedRecipes">
                 <a>X</a> | @{{recipe.title}}
+                <input type="hidden" name="recipeIds[]" value="@{{ recipe.id }}">
             </p>
         </div>
 
