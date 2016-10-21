c9d0ea43795fd36f5814d3f68d940948236ed0c2
eddcoons <emilydcoons@gmail.com>
2016-10-21 08:53:45 -0700

    added functionality for dynamic active class

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="pr-nav-lg">
     <ul>
-        <li><a href="/"><h1>Pepper Rodeo</h1></a></li>
-        <li><a href="/recipe"><i class="fa fa-cutlery"></i> Recipes</a></li>
-        <li><a href="/grocerylist"><i class="fa fa-shopping-cart"></i> Lists</a></li>
+        <li class="{{ set_active('/') }}"><a href="/"><h1>Pepper Rodeo</h1></a></li>
+        <li class="{{ set_active('recipe') }}"><a href="/recipe"><i class="fa fa-cutlery"></i> Recipes</a></li>
+        <li class="{{ set_active('grocerylist') }}"><a href="/grocerylist"><i class="fa fa-shopping-cart"></i> Lists</a></li>
         <li><a><i class="fa fa-user"></i> Account</a></li>
     </ul>
 </nav>
@@ -21,9 +21,9 @@
 
 <nav class="pr-nav">
     <ul>
-        <li><a href="/recipe"><i class="fa fa-cutlery"></i></a></li>
-        <li><a href="/grocerylist"><i class="fa fa-shopping-cart"></i></a></li>
-        <li><a href="/" class="active"><i class="fa fa-home"></i></a></li>
+        <li class="{{ set_active('recipe') }}"><a href="/recipe"><i class="fa fa-cutlery"></i></a></li>
+        <li class="{{ set_active('grocerylist') }}"><a href="/grocerylist"><i class="fa fa-shopping-cart"></i></a></li>
+        <li class="{{ set_active('/') }}"><a href="/"><i class="fa fa-home"></i></a></li>
         <li><a><i class="fa fa-user"></i></a></li>
     </ul>
 </nav>
